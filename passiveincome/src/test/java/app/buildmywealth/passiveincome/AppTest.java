13e68e902c09d12ff9bfa70d6af8342b42fc2bf2
Build My Wealth <buildmywealthapp@gmail.com>
2023-05-26 10:40:08 +0800

    Base project.

---
+++ passiveincome/src/test/java/app/buildmywealth/passiveincome/AppTest.java
@@ -0,0 +1,20 @@
+package app.buildmywealth.passiveincome;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
