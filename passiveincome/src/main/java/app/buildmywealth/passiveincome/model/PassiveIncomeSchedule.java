96ac6f7b44367d9d2ae5f489e039d78b7a785314
Build My Wealth <buildmywealthapp@gmail.com>
2023-05-27 21:47:15 +0800

    Add TDD scenario.

--- passiveincome/src/main/java/app/buildmywealth/passiveincome/model/PassiveIncomeSchedule.java
+++ passiveincome/src/main/java/app/buildmywealth/passiveincome/model/PassiveIncomeSchedule.java
@@ -4,15 +4,21 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class PassiveIncomeSchedule {
+	public static final String BROKERS_FEE = "Broker's Fee";
 	private int id;
 	private int investmentId;
+	private String description;
 	private LocalDate dateReceived;
 	private BigDecimal amount;
 	
 	public PassiveIncomeSchedule(LocalDate dateReceived, BigDecimal amount) {
+		this(dateReceived, amount, "");
+	}
+	public PassiveIncomeSchedule(LocalDate dateReceived, BigDecimal amount, String description) {
 		super();
 		this.dateReceived = dateReceived;
 		this.amount = amount;
+		this.description = description;
 	}
 	public int getId() {
 		return id;
@@ -26,6 +32,12 @@ public class PassiveIncomeSchedule {
 	public void setInvestmentId(int investmentId) {
 		this.investmentId = investmentId;
 	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	public LocalDate getDateReceived() {
 		return dateReceived;
 	}
@@ -37,5 +49,8 @@ public class PassiveIncomeSchedule {
 	}
 	public void setAmount(BigDecimal amount) {
 		this.amount = amount;
+	}
+	public boolean isBrokersFee() {
+		return BROKERS_FEE.equals(description);
 	}
 }
---
+++ passiveincome/src/test/java/app/buildmywealth/passiveincome/calculator/tdd/MonthlyPassiveIncomeCalculatorTest.java
@@ -0,0 +1,138 @@
+package app.buildmywealth.passiveincome.calculator.tdd;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.Test;
+
+import app.buildmywealth.passiveincome.model.Investment;
+import app.buildmywealth.passiveincome.model.PassiveIncomeSchedule;
+
+public class MonthlyPassiveIncomeCalculatorTest {
+	
+	private static final LocalDate TODAY = LocalDate.of(2023, 5, 30);
+
+	@Test
+	public void sameMonth() {
+		BigDecimal expectedMonthlyPassiveIncome = BigDecimal.valueOf(60_000);
+		
+		Investment investment = new Investment();
+		LocalDate sameMonth = LocalDate.of(2023, 5, 1);
+		investment.setDateCreated(sameMonth);
+		
+		List<PassiveIncomeSchedule> passiveIncomeSchedules = List.of(
+				new PassiveIncomeSchedule(LocalDate.of(2023, 5, 1), BigDecimal.valueOf(30_000)), 
+				new PassiveIncomeSchedule(LocalDate.of(2023, 5, 2), BigDecimal.valueOf(30_000)));
+		
+		CurrentDateSupplier currentDateSupplier = createMock(CurrentDateSupplier.class);
+		MonthlyPassiveIncomeCalculator monthlyPassiveIncomeCalulator = MonthlyPassiveIncomeCalculator.of(investment, passiveIncomeSchedules);
+		monthlyPassiveIncomeCalulator.setCurrentDateSupplier(currentDateSupplier);
+		
+		expect(currentDateSupplier.get()).andReturn(TODAY);
+		replay(currentDateSupplier);
+		
+		BigDecimal actualMonthlyPassiveIncome = monthlyPassiveIncomeCalulator.calculate();
+		
+		
+		assertEquals(expectedMonthlyPassiveIncome, actualMonthlyPassiveIncome);
+		verify(currentDateSupplier);
+	}
+	
+	@Test
+	public void oneMonth() {
+		BigDecimal expectedMonthlyPassiveIncome = BigDecimal.valueOf(60_000);
+		
+		Investment investment = new Investment();
+		LocalDate oneMonthAgo = LocalDate.of(2023, 4, 1);
+		investment.setDateCreated(oneMonthAgo);
+		
+		List<PassiveIncomeSchedule> passiveIncomeSchedules = List.of(
+				new PassiveIncomeSchedule(LocalDate.of(2023, 4, 1), BigDecimal.valueOf(30_000)), 
+				new PassiveIncomeSchedule(LocalDate.of(2023, 4, 2), BigDecimal.valueOf(30_000)));
+		
+		CurrentDateSupplier currentDateSupplier = createMock(CurrentDateSupplier.class);
+		MonthlyPassiveIncomeCalculator monthlyPassiveIncomeCalulator = MonthlyPassiveIncomeCalculator.of(investment, passiveIncomeSchedules);
+		monthlyPassiveIncomeCalulator.setCurrentDateSupplier(currentDateSupplier);
+		
+		expect(currentDateSupplier.get()).andReturn(TODAY);
+		replay(currentDateSupplier);
+		
+		BigDecimal actualMonthlyPassiveIncome = monthlyPassiveIncomeCalulator.calculate();
+		
+		
+		assertEquals(expectedMonthlyPassiveIncome, actualMonthlyPassiveIncome);
+		verify(currentDateSupplier);
+	}
+	
+	@Test
+	public void sixMonths() {
+		BigDecimal expectedMonthlyPassiveIncome = BigDecimal.valueOf(10_000);
+		
+		Investment investment = new Investment();
+		LocalDate sixMonthsAgo = LocalDate.of(2022, 11, 1);
+		investment.setDateCreated(sixMonthsAgo);
+		
+		List<PassiveIncomeSchedule> passiveIncomeSchedules = List.of(
+				new PassiveIncomeSchedule(LocalDate.of(2022, 11, 1), BigDecimal.valueOf(30_000)), 
+				new PassiveIncomeSchedule(LocalDate.of(2022, 11, 2), BigDecimal.valueOf(30_000)));
+		
+		CurrentDateSupplier currentDateSupplier = createMock(CurrentDateSupplier.class);
+		MonthlyPassiveIncomeCalculator monthlyPassiveIncomeCalulator = MonthlyPassiveIncomeCalculator.of(investment, passiveIncomeSchedules);
+		monthlyPassiveIncomeCalulator.setCurrentDateSupplier(currentDateSupplier);
+		
+		expect(currentDateSupplier.get()).andReturn(TODAY);
+		replay(currentDateSupplier);
+		
+		BigDecimal actualMonthlyPassiveIncome = monthlyPassiveIncomeCalulator.calculate();
+		
+		
+		assertEquals(expectedMonthlyPassiveIncome, actualMonthlyPassiveIncome);
+		verify(currentDateSupplier);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void incomeBeforeDateCreated() {
+		Investment investment = new Investment();
+		LocalDate sameMonth = LocalDate.of(2023, 5, 1);
+		investment.setDateCreated(sameMonth);
+		
+		List<PassiveIncomeSchedule> passiveIncomeSchedules = List.of(
+				new PassiveIncomeSchedule(LocalDate.of(2023, 5, 1), BigDecimal.valueOf(30_000)), 
+				new PassiveIncomeSchedule(LocalDate.of(2023, 4, 2), BigDecimal.valueOf(30_000)));
+		
+		MonthlyPassiveIncomeCalculator monthlyPassiveIncomeCalulator = MonthlyPassiveIncomeCalculator.of(investment, passiveIncomeSchedules);
+		monthlyPassiveIncomeCalulator.calculate();
+	}
+	
+	@Test
+	public void excludeBrokersFee() {
+		BigDecimal expectedMonthlyPassiveIncome = BigDecimal.valueOf(15_000);
+		
+		Investment investment = new Investment();
+		investment.setDateCreated(LocalDate.of(2023, 3, 1));
+		
+		List<PassiveIncomeSchedule> passiveIncomeSchedules = List.of(
+				new PassiveIncomeSchedule(LocalDate.of(2023, 4, 1), BigDecimal.valueOf(30_000), PassiveIncomeSchedule.BROKERS_FEE), 
+				new PassiveIncomeSchedule(LocalDate.of(2023, 5, 1), BigDecimal.valueOf(30_000)));
+		
+		CurrentDateSupplier currentDateSupplier = createMock(CurrentDateSupplier.class);
+		MonthlyPassiveIncomeCalculator monthlyPassiveIncomeCalulator = MonthlyPassiveIncomeCalculator.of(investment, passiveIncomeSchedules);
+		monthlyPassiveIncomeCalulator.setCurrentDateSupplier(currentDateSupplier);
+		
+		expect(currentDateSupplier.get()).andReturn(TODAY);
+		replay(currentDateSupplier);
+		
+		BigDecimal actualMonthlyPassiveIncome = monthlyPassiveIncomeCalulator.calculate();
+		
+		
+		assertEquals(expectedMonthlyPassiveIncome, actualMonthlyPassiveIncome);
+		verify(currentDateSupplier);
+	}
+}
